866ba6d6b3915264d72098118c19809ef5bbdb2c
Yudo <53389941+juniz@users.noreply.github.com>
2023-08-09 16:20:01 +0700

    fix error message

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -29,6 +29,10 @@
 @if ($errors->any())
 <div class="alert alert-danger">
 	<button type="button" class="close" data-dismiss="alert">×</button>
-	Form masih kosong
+	<ul>
+		@foreach ($errors->all() as $error)
+		<li>{{ $error }}</li>
+		@endforeach
+	</ul>
 </div>
 @endif
